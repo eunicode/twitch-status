7b611ddfe8fea4b30d3a1ff79e15946c65a4a3cc
Eunice Park <28845173+eunipa@users.noreply.github.com>
2018-07-16 20:46:12 -0700

    API call to get stream information for specified user

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,57 @@
-import api from '../api.js';
+import api from "../api.js";
 
 const clientID = api.clientID;
+// console.log('clientID: ', clientID);
 
-console.log('clientID: ', clientID);
+const users = [
+  "freecodecamp",
+  "noopkat",
+  "funfunfunction",
+  "cowsep",
+  "followgrubby",
+  "trikslyr"
+];
+
+// Check if user is online
+function getStatus(user) {
+  let request;
+  let response;
+
+  // Send an HTTP request
+
+  // 1. Create an XMLHttpRequest object with XHR constructor
+  request = new XMLHttpRequest();
+  console.log("XMLHttpRequest object: ", request);
+
+  // 2. Open a URL. Call open() to initialize new XMLHttpRequest object
+  // Get stream information for a specified user
+  // https://dev.twitch.tv/docs/v5/reference/streams/#get-stream-by-user
+  request.open(
+    "GET",
+    `https://api.twitch.tv/kraken/streams/${user}?client_id=${clientID}`,
+    true // async argument
+  );
+
+  // 4. If request (XMLHttpRequest transaction) completes successfully, the callback function will be called.
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      response = JSON.parse(request.responseText);
+      console.log("response: ", response);  
+
+      if (response.stream === undefined) {
+        status = "Account closed";
+
+      } else {
+        status = response.stream !== null ? "online" : "offline";
+        console.log("status: ", status);
+      }
+    }
+  };
+
+  // 3. Call send() to send the request to the server
+  request.send();
+}
+
+let status;
+
+getStatus(users[0]);
