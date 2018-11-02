1fdf58db479505c6fc196946e4629bff9b7152ad
Eunice Park <28845173+eunicode@users.noreply.github.com>
2018-11-02 10:44:00 -0700

    Updates

---
+++ api.js
@@ -0,0 +1,16 @@
+export default
+{
+    clientID: "2hz90m0uh0nput0qtr8qwc9245maj9"
+}
+
+// Alternative export method
+
+// export const api = {
+//     clientID: "2hz90m0uh0nput0qtr8qwc9245maj9"
+// }
+
+// import {api} from '../api.js';
+
+// Sample API calls
+// https://api.twitch.tv/kraken/streams/freecodecamp?client_id=2hz90m0uh0nput0qtr8qwc9245maj9
+// https://api.twitch.tv/kraken/channels/freecodecamp?client_id=2hz90m0uh0nput0qtr8qwc9245maj9
---
+++ js/main.js
@@ -0,0 +1,128 @@
+import api from "../api.js";
+
+const clientID = api.clientID;
+// console.log('clientID: ', clientID);
+
+const users = [
+  "freecodecamp",
+  "noopkat",
+  "funfunfunction",
+  "cowsep",
+  "followgrubby",
+  "trikslyr"
+];
+
+let main = document.querySelector("main");
+
+// STEP 2
+// Check if user is online
+function getStatus(user) {
+  let request;
+  let response;
+  let status;
+
+  // Send an HTTP request
+
+  // 1. Create an XMLHttpRequest object with XHR constructor
+  request = new XMLHttpRequest();
+  console.log("XMLHttpRequest object: ", request);
+
+  // 2. Open a URL. Call open() to initialize new XMLHttpRequest object
+  // Get stream information for a specified user
+  // https://dev.twitch.tv/docs/v5/reference/streams/#get-stream-by-user
+  request.open(
+    "GET",
+    `https://api.twitch.tv/kraken/streams/${user}?client_id=${clientID}`,
+    true // async argument
+  );
+
+  // 4. If request (XMLHttpRequest transaction) completes successfully, the callback function will be called.
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      response = JSON.parse(request.responseText);
+      console.log("response: ", response);  
+
+      if (response.stream === undefined) {
+        status = "closed";
+
+      } else {
+        status = response.stream !== null ? "online" : "offline";
+        console.log("status: ", status);
+      }
+    }
+
+    getChannel(user, status);
+  };
+
+  // 3. Call send() to send the request to the server
+  request.send();
+}
+
+// STEP 3
+function getChannel(user, status) {
+    let request;
+    let response;
+    let channelStatus;
+    // let channelName;
+    let channelUrl;
+    let channelLogo;
+    let channelGame;
+
+    request = new XMLHttpRequest();
+
+    // https://dev.twitch.tv/docs/v5/reference/channels/#get-channel-by-id
+    request.open(
+        "GET", 
+        `https://api.twitch.tv/kraken/channels/${user}?client_id=${clientID}`,
+        true
+    );
+
+    request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+            response = JSON.parse(request.responseText);
+            console.log("response: ", response);
+            // channelName = response.display_name;
+            channelUrl = response.url;
+            channelLogo = response.logo;
+            channelGame = response.game;
+
+            if (status === "online") {
+                channelStatus = `online: ${response.status}`;
+            } else if (status === "offline") {
+                channelStatus = "offline";
+            } else {
+                channelStatus = "closed";
+            }
+            
+            main.innerHTML = main.innerHTML + 
+            `<section class="item">
+            <img src=${channelLogo} alt="logo" class="item-img">
+            <div class="item-right">
+                <h3><a href=${channelUrl} target="_blank">${user}</a></h3>
+                <p>${channelStatus}</p>
+                <p class="item-subtext">${channelGame}</p>
+            </div>
+            </section>`
+
+        }
+    }
+
+    request.send();
+}
+
+// STEP 1
+for (let i = 0; i < users.length; i++) {
+    getStatus(users[i]);
+}
+
+// TO DO 
+// Get rid of global variables
+// Promises
+// DRY up code with OOJS?
+// Sticky footer (in the case of zero users)
+// Add BEM classes
+// Add colored dot to signal if user is online/offline
+// Add color to username to signal if user is online/offline
+// Sort online users to the top
+// Add tabs for offline, online users
+// Add ability to search and add/remove users
