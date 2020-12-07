7fc64dcc577f81e37667e8f783fd4b489cab834a
Eunice Park <28845173+eunicode@users.noreply.github.com>
2020-12-06 21:39:40 -0800

    Updates

---
+++ data/docs-example.js
@@ -0,0 +1,160 @@
+{
+  "data": [
+    {
+      "broadcaster_language": "en",
+      "display_name": "a_seagull",
+      "game_id": "513001",
+      "id": "19070311",
+      "is_live": false,
+      "tag_ids": [],
+      "thumbnail_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/a_seagull-profile_image-4d2d235688c7dc66-300x300.png",
+      "title": "THIS GAME ANY GOOD? || trying ubi's botw-ish game",
+      "started_at": ""
+    },
+    {
+      "broadcaster_language": "",
+      "display_name": "a_seagull__",
+      "game_id": "0",
+      "id": "603921128",
+      "is_live": false,
+      "tag_ids": [],
+      "thumbnail_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/183e7a8d-a487-4309-9ec7-33e9deb8c19f-profile_image-300x300.png",
+      "title": "",
+      "started_at": ""
+    },
+    {
+      "broadcaster_language": "en",
+      "display_name": "fundamentally_a_seagull",
+      "game_id": "512709",
+      "id": "438322552",
+      "is_live": false,
+      "tag_ids": [],
+      "thumbnail_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/75f246a1-88c0-481b-9ccd-9f99eab6e944-profile_image-300x300.png",
+      "title": "idk its just cod Cold war",
+      "started_at": ""
+    },
+    {
+      "broadcaster_language": "",
+      "display_name": "a_seagullrf",
+      "game_id": "0",
+      "id": "594078796",
+      "is_live": false,
+      "tag_ids": [],
+      "thumbnail_url": "https://static-cdn.jtvnw.net/user-default-pictures-uv/ebe4cd89-b4f4-4cd9-adac-2f30151b4209-profile_image-300x300.png",
+      "title": "",
+      "started_at": ""
+    },
+    {
+      "broadcaster_language": "",
+      "display_name": "a_seagulljr",
+      "game_id": "0",
+      "id": "593850060",
+      "is_live": false,
+      "tag_ids": [],
+      "thumbnail_url": "https://static-cdn.jtvnw.net/user-default-pictures-uv/ebb84563-db81-4b9c-8940-64ed33ccfc7b-profile_image-300x300.png",
+      "title": "",
+      "started_at": ""
+    },
+    {
+      "broadcaster_language": "en",
+      "display_name": "a_seagulllc",
+      "game_id": "0",
+      "id": "591916883",
+      "is_live": false,
+      "tag_ids": [],
+      "thumbnail_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/9018393d-cf54-4f99-870e-136610c3e6fa-profile_image-300x300.png",
+      "title": "",
+      "started_at": ""
+    },
+    {
+      "broadcaster_language": "en",
+      "display_name": "a_seagullgj",
+      "game_id": "0",
+      "id": "591863289",
+      "is_live": false,
+      "tag_ids": [],
+      "thumbnail_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/ee2558d5-6fcd-408e-a836-9f7ab5d6ee5a-profile_image-300x300.png",
+      "title": "",
+      "started_at": ""
+    },
+    {
+      "broadcaster_language": "",
+      "display_name": "a_seagullmh",
+      "game_id": "0",
+      "id": "588277555",
+      "is_live": false,
+      "tag_ids": [],
+      "thumbnail_url": "https://static-cdn.jtvnw.net/user-default-pictures-uv/dbdc9198-def8-11e9-8681-784f43822e80-profile_image-300x300.png",
+      "title": "",
+      "started_at": ""
+    },
+    {
+      "broadcaster_language": "en",
+      "display_name": "a_seagullei",
+      "game_id": "0",
+      "id": "588211034",
+      "is_live": false,
+      "tag_ids": [],
+      "thumbnail_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/4c76689a-7447-4b6d-85ed-da28705d51a8-profile_image-300x300.png",
+      "title": "",
+      "started_at": ""
+    },
+    {
+      "broadcaster_language": "",
+      "display_name": "a_seagullnu",
+      "game_id": "0",
+      "id": "579536616",
+      "is_live": false,
+      "tag_ids": [],
+      "thumbnail_url": "https://static-cdn.jtvnw.net/user-default-pictures-uv/de130ab0-def7-11e9-b668-784f43822e80-profile_image-300x300.png",
+      "title": "",
+      "started_at": ""
+    },
+    {
+      "broadcaster_language": "",
+      "display_name": "a_seagulltx",
+      "game_id": "0",
+      "id": "578660775",
+      "is_live": false,
+      "tag_ids": [],
+      "thumbnail_url": "https://static-cdn.jtvnw.net/user-default-pictures-uv/41780b5a-def8-11e9-94d9-784f43822e80-profile_image-300x300.png",
+      "title": "",
+      "started_at": ""
+    },
+    {
+      "broadcaster_language": "",
+      "display_name": "a_seagulluk",
+      "game_id": "0",
+      "id": "578660165",
+      "is_live": false,
+      "tag_ids": [],
+      "thumbnail_url": "https://static-cdn.jtvnw.net/user-default-pictures-uv/75305d54-c7cc-40d1-bb9c-91fbe85943c7-profile_image-300x300.png",
+      "title": "",
+      "started_at": ""
+    }
+  ],
+  "pagination": {}
+}
+
+/*
+https://dev.twitch.tv/docs/api
+
+Get streams
+{
+  data:[ { stream_data }],
+  pagination: {} 
+}
+
+game_id: "508402"
+game_name: "Live"
+id: "40217227276"
+language: "ro"
+started_at: "2020-12-03T13:45:24Z"
+tag_ids: (2) ["75a99c80-0f15-4159-b1fd-3812c25b4aca", "77017b7b-6b98-4f0d-bd39-3ba96b66ec7a"]
+thumbnail_url: "https://static-cdn.jtvnw.net/previews-ttv/live_user_cnstv1994-{width}x{height}.jpg"
+title: "CNS TV (STREAM) LIVE!"
+type: "live"
+user_id: "274890004"
+user_name: "cnstv1994"
+viewer_count: 2
+ */
--- js/main.js
+++ js/main.js
@@ -1,134 +1,229 @@
-import api from "../api.js";
+import { auth } from "../secrets.js";
 
-// Bad global variables
-
-const clientID = api.clientID;
-// console.log('clientID: ', clientID);
-
-const users = [
+// Bad global variable
+const USERS = [
   "freecodecamp",
   "noopkat",
   "funfunfunction",
   "jsthegame",
+  "kentcdodds",
   "cscareerhackers",
-  "cowsep",
-  "followgrubby",
-  "trikslyr"
+  "riotgames",
+  "tfue",
+  "myth",
 ];
 
-let main = document.querySelector("main");
+// Bad global variable
+let ept = {
+  CLIENT_ID: auth.clientId,
+  URL_STREAM: "https://api.twitch.tv/helix/streams",
+  URL_USER: "https://api.twitch.tv/helix/users",
+  URL_AUTH: "https://id.twitch.tv/oauth2/authorize",
+  URL_REDIRECT: "http://localhost:3000",
+  // URL_REDIRECT = 'https://eunicode.github.io/twitch-status/',
+  ACCESS_TOKEN: "",
+  INIT_OBJ: "",
+};
 
-// STEP 2
-// Check if user is online
-function getStatus(user) {
-  let request;
-  let response;
-  let status;
+// Bad global variable
+let nodeDummy = document.querySelector("#main-dummy");
+let nodeApi = document.querySelector("#main-api");
 
-  // Send an HTTP request
+// Build dummy grid
+sequentialAsyncTaskWrapper(
+  "../data/simulatedDataUser.json",
+  "../data/simulatedDataStream.json",
+  nodeDummy
+);
 
-  // 1. Create an XMLHttpRequest object with XHR constructor
-  request = new XMLHttpRequest();
-  console.log("XMLHttpRequest object: ", request);
-
-  // 2. Open a URL. Call open() to initialize new XMLHttpRequest object
-  // Get stream information for a specified user
-  // https://dev.twitch.tv/docs/v5/reference/streams/#get-stream-by-user
-  request.open(
-    "GET",
-    `https://api.twitch.tv/kraken/streams/${user}?client_id=${clientID}`,
-    true // async argument
+// Set login button's link to Twitch login URL
+document
+  .querySelector("#a-auth")
+  .setAttribute(
+    "href",
+    `${ept.URL_AUTH}?client_id=${
+      ept.CLIENT_ID
+    }&redirect_uri=${encodeURIComponent(ept.URL_REDIRECT)}&response_type=token`
   );
 
-  // 4. If request (XMLHttpRequest transaction) completes successfully, the callback function will be called.
-  request.onload = function() {
-    if (request.status >= 200 && request.status < 400) {
-      response = JSON.parse(request.responseText);
-      console.log("response: ", response);  
+// If user chooses to authenticate, they get a URL like http://localhost:3000/#access_token=11111&scope=&token_type=bearer
+// Location.hash returns a string containing a '#' followed by the fragment identifier of the URL
+if (document.location.hash) {
+  // Get access token from URL
+  let accessToken = getAccessToken();
+  ept.ACCESS_TOKEN = accessToken; // make access_token "global"
 
-      if (response.stream === undefined) {
-        status = "closed";
+  if (accessToken) {
+    // Hide dummy grid
+    let section = document.querySelector("#main-dummy");
+    let dummyNotice = document.querySelector("#text-dummy-notice");
+    hideDom(section);
+    hideDom(dummyNotice);
 
-      } else {
-        status = response.stream !== null ? "online" : "offline";
-        console.log("status: ", status);
-      }
+    // Iterate array and run code for each element
+    let urlUser = `${ept.URL_USER}?`;
+    let urlStream = `${ept.URL_STREAM}?`;
+
+    // Use one API call to get data for multiple users
+    for (const user of USERS) {
+      urlUser += `&login=${user}`;
+      urlStream += `&user_login=${user}`;
     }
 
-    getChannel(user, status);
-  };
-
-  // 3. Call send() to send the request to the server
-  request.send();
+    sequentialAsyncTaskWrapper(urlUser, urlStream, nodeApi);
+  }
 }
 
-// STEP 3
-function getChannel(user, status) {
-    let request;
-    let response;
-    let channelStatus;
-    // let channelName;
-    let channelUrl;
-    let channelLogo;
-    let channelGame;
+async function sequentialAsyncTaskWrapper(urlUser, urlStream, node) {
+  // Alternatively, use rest syntax to receive any number of args
+  let userData = await fetchJson(urlUser); // Step 1. These are sequential
+  let streamData = await fetchJson(urlStream); // Step 2.
+  let streamSet = new Set();
 
-    request = new XMLHttpRequest();
+  // Create set of online users, O(1) lookup
+  for (let streamer of streamData.data) {
+    streamSet.add(streamer.user_name.toLowerCase());
+  }
 
-    // https://dev.twitch.tv/docs/v5/reference/channels/#get-channel-by-id
-    request.open(
-        "GET", 
-        `https://api.twitch.tv/kraken/channels/${user}?client_id=${clientID}`,
-        true
+  for (let user of userData.data) {
+    buildDom(user, streamSet, node);
+  }
+
+  // Alternatively, use `await Promise.all(asyncTask1(), asyncTask2())`
+}
+
+/* -------------------------------------------------------------------------- */
+// HELPER FUNCTIONS
+
+function getAccessToken() {
+  let parsedHash = new URLSearchParams(window.location.hash.substr(1)); // Get string after '#'
+  let accessToken = parsedHash.get("access_token");
+  return accessToken;
+}
+
+function domLoadingMessage() {
+  // document.querySelector("#main-api").textContent = "Loading";
+}
+
+async function fetchJson(url) {
+  const response = await fetch(
+    `${url}`, // resource
+    // ept.URL_STREAM + "?user_login=cnstv1994",
+    {
+      headers: {
+        // `init` object
+        "Client-ID": ept.CLIENT_ID,
+        Authorization: `Bearer ${ept.ACCESS_TOKEN}`,
+      },
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error(
+      `API request failed. HTTP response status code: ${response.status}`
     );
+  } else {
+    let responseJson = await response.json();
+    // console.log(responseJson);
+    return responseJson;
+  }
+}
 
-    request.onload = function() {
-        if (request.status >= 200 && request.status < 400) {
-            response = JSON.parse(request.responseText);
-            console.log("response: ", response);
-            // channelName = response.display_name;
-            channelUrl = response.url;
-            channelLogo = response.logo;
-            channelGame = response.game;
+function buildDom(userData, userStatus, parentNode) {
+  let userName = userData.login;
+  let userOnlineStatus = userStatus.has(userName.toLowerCase())
+    ? "live"
+    : "offline";
+  let userUrl = `https://twitch.tv/${userData.login}`;
+  let userIcon = userData.profile_image_url;
+  let userDescription = userData.description;
 
-            if (status === "online") {
-                channelStatus = `online: ${response.status}`;
-            } else if (status === "offline") {
-                channelStatus = "offline";
-            } else {
-                channelStatus = "closed";
-            }
-            
-            main.innerHTML = main.innerHTML + 
-            `<section class="item">
-            <img src=${channelLogo} alt="logo" class="item-img">
+  parentNode.innerHTML =
+    parentNode.innerHTML +
+    `<section class="item">
+            <img src=${userIcon} alt="user icon" class="item-img">
             <div class="item-right">
-                <h3><a href=${channelUrl} target="_blank">${user}</a></h3>
-                <p>${channelStatus}</p>
-                <p class="item-subtext">${channelGame}</p>
+                <h3><a href=${userUrl} target="_blank">${userName}</a></h3>
+                <p class=${
+                  userOnlineStatus === "live" ? "text-online" : "text-offline"
+                }>${userOnlineStatus}</p>
+                <p class="item-subtext">${userDescription}</p>
             </div>
-            </section>`
-
-        }
-    }
-
-    request.send();
+            </section>`;
 }
 
-// STEP 1
-for (let i = 0; i < users.length; i++) {
-    getStatus(users[i]);
+function hideDom(node) {
+  if (!node.classList.contains("hide")) {
+    node.classList.add("hide");
+  }
+
+  // We need to get rid of .main-grid bc further down the css file, .main-grid sets display to "grid", which overwrites "none"
+  if (node.classList.contains("main-grid")) {
+    node.classList.remove("main-grid");
+  }
+
+  
+
+
 }
 
-// TO DO 
-// Get rid of global variables
-// Promises
-// DRY up code with OOJS?
-// Add BEM classes
-// Add colored dot to signal if user is online/offline
-// Add color to username to signal if user is online/offline
-// Sort online users to the top
-// Add tabs for offline, online users
-// Add ability to search and add/remove users
+/* -------------------------------------------------------------------------- */
+
+/*
+TO DO
+Get rid of global variables
+Promises
+DRY up code with OOJS?
+Add BEM classes
+Add colored dot to signal if user is online/offline
+Add color to username to signal if user is online/offline
+Sort online users to the top
+Add tabs for offline, online users
+Add ability to search and add/remove users
+
+TO DO MORE
+Store user access token in local storage 
+*/
 
 // DONE
 // Sticky footer (in the case of zero users)
+
+/* -------------------------------------------------------------------------- */
+
+/*
+
+https://dev.twitch.tv/docs/authentication
+
+Authentication involves:
+1. Registering your app to obtain a client ID and client secret.
+2. Getting a token. This includes specifying scopes, the permissions your app requires.
+3. Sending the token in your API request, to authenticate API requests.
+
+User access tokens = Authenticate users and allow your app to make requests on their behalf. If your application uses Twitch for login or makes requests in the context of an authenticated user, you need to generate a user access token.
+
+authentication flows
+Implicit code flow	= Your app does not use a server, such as a client-side JavaScript app or mobile app. This approach does not require a server that must make requests to the API.
+Authorization code flow	= Your application uses a server, can securely store a client secret, and can make server-to-server requests.
+Client credentials flow	= You need an app access token.
+
+OAuth implicit code flow
+https://dev.twitch.tv/docs/authentication/getting-tokens-oauth#oauth-implicit-code-flow
+
+--------------------------------------------------------------------------------
+Location: hash
+https://developer.mozilla.org/en-US/docs/Web/API/Location/hash
+
+URLSearchParams
+https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams
+
+The URLSearchParams interface defines utility methods to work with the query string of a URL.
+
+URLSearchParams.get()
+https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams/get
+
+--------------------------------------------------------------------------------
+Twitch implicit auth example
+https://barrycarlyon.github.io/twitch_misc/authentication/implicit_auth/
+https://github.com/BarryCarlyon/twitch_misc/blob/master/authentication/implicit_auth/index.html
+
+ */
